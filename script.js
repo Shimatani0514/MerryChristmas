b2a3cdc034ad4bd746b8ea2506dc1730cba1f9ab
Shimatani0514 <shimatani0514@outlook.jp>
2023-12-24 22:50:54 +0900

    create switch

--- script.js
+++ script.js
@@ -26,3 +26,54 @@ const createSnow = () => {
 
 setInterval(createSnow, 100);
 
+
+
+let isIlluminating = false;
+
+document.getElementById("ligt-up").addEventListener('click', function() {
+    const container = document.querySelector(".lights");
+
+    const lightCountY = 200; // 生成する黄色の光の数
+    const lightCountB = 200; // 生成する青色の光の数
+    const lightCountR = 200; // 生成する赤色の光の数
+
+    if (!isIlluminating) { // イルミネーションがオフの場合、開始する
+        isIlluminating = true;
+
+        function createLights(count, className) {
+            for (let i = 0; i < count; i++) {
+                const light = document.createElement('span');
+                light.classList.add(className);
+                container.appendChild(light);
+
+                light.style.left = Math.random() * 70 + 30 + "%";
+                light.style.top = Math.random() * 45 + 0 + "%";
+
+                light.classList.add('illumination');
+            }
+        }
+
+        createLights(lightCountY, 'light-y');
+        createLights(lightCountB, 'light-b');
+        createLights(lightCountR, 'light-r');
+    } else { // イルミネーションがオンの場合、停止する
+        isIlluminating = false;
+
+        const lightsY = document.querySelectorAll('.light-y');
+        const lightsB = document.querySelectorAll('.light-b');
+        const lightsR = document.querySelectorAll('.light-r');
+
+        lightsY.forEach(light => {
+            container.removeChild(light);
+        });
+        lightsB.forEach(light => {
+            container.removeChild(light);
+        });
+        lightsR.forEach(light => {
+            container.removeChild(light);
+        });
+    }
+});
+
+
+
